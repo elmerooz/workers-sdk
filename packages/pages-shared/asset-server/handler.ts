c5703de8b75ee8f80aa73245516230ef3f96db32
Jacob Hands <jacob@gogit.io>
2024-04-22 17:34:35 -0500

    fixup! feedback: Simplify logic

--- packages/pages-shared/asset-server/handler.ts
+++ packages/pages-shared/asset-server/handler.ts
@@ -554,32 +554,25 @@ export async function generateHandler<
 								ASSET_PRESERVATION_CACHE_V2
 							);
 
-							let shouldUpdateCache = true;
-
 							// Check if the asset has changed since last written to cache
 							const match = await assetPreservationCacheV2.match(request);
 							if (match) {
-								const cachedAssetKey = await match.text();
-								if (cachedAssetKey === assetKey) {
-									shouldUpdateCache = false;
+								if (assetKey !== (await match.text())) {
+									// cache the asset key in the cache with all the headers.
+									// When we read it back, we'll re-fetch the body but use the
+									// cached headers.
+									const preservedResponse = new Response(assetKey, response);
+									preservedResponse.headers.set(
+										"cache-control",
+										CACHE_CONTROL_PRESERVATION
+									);
+									preservedResponse.headers.set("x-robots-tag", "noindex");
+
+									await assetPreservationCacheV2.put(
+										getCacheKey(),
+										preservedResponse
+									);
 								}
-							}
-
-							if (shouldUpdateCache) {
-								// cache the asset key in the cache with all the headers.
-								// When we read it back, we'll re-fetch the body but use the
-								// cached headers.
-								const preservedResponse = new Response(assetKey, response);
-								preservedResponse.headers.set(
-									"cache-control",
-									CACHE_CONTROL_PRESERVATION
-								);
-								preservedResponse.headers.set("x-robots-tag", "noindex");
-
-								await assetPreservationCacheV2.put(
-									getCacheKey(),
-									preservedResponse
-								);
 							}
 						} catch (err) {
 							logError(err as Error);
